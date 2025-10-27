8f7426858c00cbcbaa9343deeb6212361551d565
litlighilit <litlighilit@foxmail.com>
2025-10-27 16:34:30 +0800

    fix(keymap): <C-Del> not correct action

--- lua/gui_editor_like_keymaps.lua
+++ lua/gui_editor_like_keymaps.lua
@@ -3,6 +3,8 @@
 
 vim.keymap.set('i', '<C-BS>', '<C-w>', { noremap = true, desc = "Delete previous word" })
 
-vim.keymap.set('i', '<C-Del>', '<C-o>dw<Del>', { noremap = true, desc = "Delete next word" })
+vim.keymap.set('i', '<C-Del>', '<C-o>de', { noremap = true, desc = "Delete next word" })
 
+--vim.keymap.set('i', '<C-Z>', '<C-o>u', { noremap = true, desc = "undo" })
+--vim.keymap.set('i', '<C-S-Z>', '<C-o><C-R>', { noremap = true, desc = "redo" })
 
